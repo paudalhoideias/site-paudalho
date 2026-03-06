c87b928ede64c8f5af30787cc0efbb0d7e0c1acd
paudalhoideias <paudalhoideias@gmail.com>
2026-03-06 09:02:02 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-// scripts do site
+function toggleMenu(){
+
+const menu = document.getElementById("menu");
+
+if(menu.style.display === "flex"){
+menu.style.display = "none";
+}else{
+menu.style.display = "flex";
+}
+
+}
